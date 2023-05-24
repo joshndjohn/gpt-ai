13060d32f1eae38e25fe8e979dc8dd7d13535ee1
joshndjohn <joshndjohn@gmail.com>
2023-05-24 09:21:27 +0100

    Folders and files structure setup

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './brand.css'; 
+
+const Brand = () => {
+  return (
+    <div>Brand</div>
+  )
+}
+
+export default Brand
+
+
+//Hold the alt key and double click anywhere the thing you want to change is mentioned 
+//Hold control or command and "D" at the same time to select all intances of what you want to edit
